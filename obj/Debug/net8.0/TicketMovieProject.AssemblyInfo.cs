5a4d976c610d4d19213a900e11b2a250e7391d12
dat <vnqd0301@gmail.com>
2024-12-03 11:07:18 +0700

    fixed render UIFrameworkAttribute(Bootstrap5)

--- obj/Debug/net8.0/TicketMovieProject.AssemblyInfo.cs
+++ obj/Debug/net8.0/TicketMovieProject.AssemblyInfo.cs
@@ -11,11 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
+//[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketMovieProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f97acb9a18ba67836637ae8781cd529e44f4bb8f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87259c9e7fc74aafc47036576c44911f9c993903")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketMovieProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketMovieProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
