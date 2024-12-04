d3f56fb87723ebe7f64376e513df2002db3003ef
QuocDat <vnqd0301@gmail.com>
2024-12-04 19:54:31 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+﻿using ASPNetCoreRazorPage_TicketMovie.Models;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.EntityFrameworkCore;
+
 namespace ASPNetCoreRazorPage_TicketMovie
 {
     public class Program
@@ -8,6 +14,69 @@ namespace ASPNetCoreRazorPage_TicketMovie
 
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            // Thêm dịch vụ bảo mật CORS
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAll", policy =>
+                {
+                    policy.AllowAnyOrigin()  // Cho phép mọi domain
+                          .AllowAnyMethod()  // Cho phép mọi HTTP method (GET, POST, PUT, DELETE, ...)
+                          .AllowAnyHeader(); // Cho phép mọi header
+                });
+            });
+
+            // Kết nối tới Database
+            builder.Services.AddDbContext<AppDataContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnline"));
+            });
+
+            //builder.Services.AddDefaultIdentity<AuthUser>(options => options.SignIn.RequireConfirmedAccount = false)
+            //    .AddRoles<IdentityRole>()
+            //    .AddEntityFrameworkStores<AppDataContext>();
+
+            // Ràng buộc khi đăng kí tài khoản
+            builder.Services.Configure<IdentityOptions>(options => {
+                // Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
+
+                // Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30); // Khóa 30 phút
+                options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
+
+                // Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+                // Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = false;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+            });
+
+            // Tăng giới hạn form
+            builder.Services.Configure<FormOptions>(options =>
+            {
+                options.ValueCountLimit = 2000; // Số lượng giá trị cho phép lớn hơn 1024
+                options.MultipartBodyLengthLimit = 268435456; // Tăng kích thước tối đa của body (256MB)
+            });
+
+            // Tăng giới hạn kích thước yêu cầu (request size limits)
+            builder.Services.Configure<IISServerOptions>(options =>
+            {
+                options.MaxRequestBodySize = 268435456; // Tăng kích thước tối đa của body (256MB)
+            });
+            builder.Services.Configure<KestrelServerOptions>(options =>
+            {
+                options.Limits.MaxRequestBodySize = 268435456; // Tăng kích thước tối đa của body (256MB)
+            });
 
             var app = builder.Build();
 
@@ -23,6 +92,7 @@ namespace ASPNetCoreRazorPage_TicketMovie
             app.UseStaticFiles();
 
             app.UseRouting();
+            app.UseCors("AllowSpecificOrigin");
 
             app.UseAuthorization();
 
