0f2e172f99f63dd750cf14b89b9fbc5be5eb1992
QuocDat <vnqd0301@gmail.com>
2024-12-07 15:09:07 +0700

    Scaffolder Login, Logout, Register, setup required to register

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using ASPNetCoreRazorPage_TicketMovie.Models;
+using ASPNetCoreRazorPage_TicketMovie.Models;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
@@ -35,31 +35,32 @@ namespace ASPNetCoreRazorPage_TicketMovie
                           .AllowAnyHeader(); // Cho phép mọi header
                 });
             });
-            
-            // Ràng buộc khi đăng kí tài khoản
-            //builder.Services.Configure<IdentityOptions>(options => {
-            //     Thiết lập về Password
-            //    options.Password.RequireDigit = false; // Không bắt phải có số
-            //    options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
-            //    options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
-            //    options.Password.RequireUppercase = false; // Không bắt buộc chữ in
-            //    options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
-            //    options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
 
-            //     Cấu hình Lockout - khóa user
-            //    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30); // Khóa 30 phút
-            //    options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
-            //    options.Lockout.AllowedForNewUsers = true;
+            //Ràng buộc khi đăng kí tài khoản
+            builder.Services.Configure<IdentityOptions>(options =>
+            {
+                //Thiết lập về Password
+                options.Password.RequireDigit = false; // Không bắt phải có số
+                options.Password.RequireLowercase = false; // Không bắt phải có chữ thường
+                options.Password.RequireNonAlphanumeric = false; // Không bắt ký tự đặc biệt
+                options.Password.RequireUppercase = false; // Không bắt buộc chữ in
+                options.Password.RequiredLength = 3; // Số ký tự tối thiểu của password
+                options.Password.RequiredUniqueChars = 1; // Số ký tự riêng biệt
 
-            //     Cấu hình về User.
-            //    options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
-            //        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
-            //    options.User.RequireUniqueEmail = true;  // Email là duy nhất
+                //Cấu hình Lockout - khóa user
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(30); // Khóa 30 phút
+                options.Lockout.MaxFailedAccessAttempts = 5; // Thất bại 5 lầ thì khóa
+                options.Lockout.AllowedForNewUsers = true;
 
-            //     Cấu hình đăng nhập.
-            //    options.SignIn.RequireConfirmedEmail = false;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
-            //    options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
-            //});
+                //Cấu hình về User.
+                options.User.AllowedUserNameCharacters = // các ký tự đặt tên user
+                    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                options.User.RequireUniqueEmail = true;  // Email là duy nhất
+
+                //Cấu hình đăng nhập.
+                options.SignIn.RequireConfirmedEmail = false;            // Cấu hình xác thực địa chỉ email (email phải tồn tại)
+                options.SignIn.RequireConfirmedPhoneNumber = false;     // Xác thực số điện thoại
+            });
 
             // Tăng giới hạn form
             builder.Services.Configure<FormOptions>(options =>
