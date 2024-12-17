5b156b6ed2081beee27021e72be93e907b556a13
QuocDat <vnqd0301@gmail.com>
2024-12-17 14:25:34 +0700

    Connect data to admin

--- Pages/Admin/Index.cshtml.cs
+++ Pages/Admin/Index.cshtml.cs
@@ -1,12 +1,24 @@
+using ASPNetCoreRazorPage_TicketMovie.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace ASPNetCoreRazorPage_TicketMovie.Pages.Admin
 {
+    [Authorize(Roles = "admin")]
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly UserManager<UserOA> _userManager;
+        private readonly AppDataContext _context;
+
+        public IndexModel(AppDataContext context, UserManager<UserOA> userManager) {
+            _context = context;
+            _userManager = userManager;
+        }
+        public async Task OnGetAsync()
         {
+            var user = await _userManager.GetUserAsync(User);
         }
     }
 }
--- Pages/Admin/Movie.cshtml.cs
+++ Pages/Admin/Movie.cshtml.cs
@@ -1,12 +1,24 @@
+using ASPNetCoreRazorPage_TicketMovie.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ASPNetCoreRazorPage_TicketMovie.Pages.Admin
 {
+    [Authorize(Roles = "admin")]
     public class MovieModel : PageModel
     {
-        public void OnGet()
+        private readonly AppDataContext _context;
+        public IList<Movie>? Movies { get; set; }
+        public MovieModel(AppDataContext context)
         {
+            _context = context;
+            Movies = new List<Movie>();
+        }
+        public async Task OnGetAsync()
+        {
+            Movies = await _context.Movies.Include(x => x.Category).AsNoTracking().ToListAsync();
         }
     }
 }
